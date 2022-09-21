d541a151aac4d2df343af6be0ef47d254b1c073f
Krzysztof Kwapisiewicz <sq2lyk@gmail.com>
2022-09-21 15:57:35 +0200

    Aperturize OTLP Receiver
    This commit makes OTLP receiver usable with Aperture platform code,
    i.e. reusing already existing GRPC server in
    https://github.com/fluxninja/aperture.

--- receiver/otlpreceiver/config.go
+++ receiver/otlpreceiver/config.go
@@ -15,70 +15,15 @@
 package otlpreceiver // import "go.opentelemetry.io/collector/receiver/otlpreceiver"
 
 import (
-	"errors"
-
 	"go.opentelemetry.io/collector/config"
-	"go.opentelemetry.io/collector/config/configgrpc"
-	"go.opentelemetry.io/collector/config/confighttp"
-	"go.opentelemetry.io/collector/confmap"
 )
-
-const (
-	// Protocol values.
-	protoGRPC          = "grpc"
-	protoHTTP          = "http"
-	protocolsFieldName = "protocols"
-)
-
-// Protocols is the configuration for the supported protocols.
-type Protocols struct {
-	GRPC *configgrpc.GRPCServerSettings `mapstructure:"grpc"`
-	HTTP *confighttp.HTTPServerSettings `mapstructure:"http"`
-}
 
 // Config defines configuration for OTLP receiver.
 type Config struct {
 	config.ReceiverSettings `mapstructure:",squash"` // squash ensures fields are correctly decoded in embedded struct
-	// Protocols is the configuration for the supported protocols, currently gRPC and HTTP (Proto and JSON).
-	Protocols `mapstructure:"protocols"`
+	traceServerWrapper      *TraceServerWrapper
+	metricServerWrapper     *MetricServerWrapper
+	logServerWrapper        *LogServerWrapper
 }
 
 var _ config.Receiver = (*Config)(nil)
-var _ confmap.Unmarshaler = (*Config)(nil)
-
-// Validate checks the receiver configuration is valid
-func (cfg *Config) Validate() error {
-	if cfg.GRPC == nil &&
-		cfg.HTTP == nil {
-		return errors.New("must specify at least one protocol when using the OTLP receiver")
-	}
-	return nil
-}
-
-// Unmarshal a confmap.Conf into the config struct.
-func (cfg *Config) Unmarshal(componentParser *confmap.Conf) error {
-	if componentParser == nil || len(componentParser.AllKeys()) == 0 {
-		return errors.New("empty config for OTLP receiver")
-	}
-	// first load the config normally
-	err := componentParser.Unmarshal(cfg, confmap.WithErrorUnused())
-	if err != nil {
-		return err
-	}
-
-	// next manually search for protocols in the confmap.Conf, if a protocol is not present it means it is disabled.
-	protocols, err := componentParser.Sub(protocolsFieldName)
-	if err != nil {
-		return err
-	}
-
-	if !protocols.IsSet(protoGRPC) {
-		cfg.GRPC = nil
-	}
-
-	if !protocols.IsSet(protoHTTP) {
-		cfg.HTTP = nil
-	}
-
-	return nil
-}
